fdca4d62f0392868fd521b1dc6b0bf887f0b9bd4
@Vitorgabriel159 <vitor.gabriel@prodigasistemas.com.br>
2025-11-12 11:29:01 -0300

    teste

--- app/src/main/java/br/com/prodiga/cursocompose/MainActivity.kt
+++ app/src/main/java/br/com/prodiga/cursocompose/MainActivity.kt
@@ -1,16 +1,33 @@
 package br.com.prodiga.cursocompose
 
 import android.os.Bundle
+import android.text.Layout
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.carousel.HorizontalMultiBrowseCarousel
+import androidx.compose.material3.carousel.rememberCarouselState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.LineHeightStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import br.com.prodiga.cursocompose.ui.theme.CURSOComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,10 +37,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             CURSOComposeTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                   testeLogo(modifier = Modifier.padding(       innerPadding    ))
                 }
             }
         }
@@ -31,17 +45,46 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun CarouselExample_MultiBrowse() {
+    data class CarouselItem(
+        val id: Int,
+        @DrawableRes val imageResId: Int,
+        val contentDescription: String
     )
-}
 
+    val items = remember {
+        listOf(
+            CarouselItem(0, R.drawable.ic_launcher_background, "cupcake"),
+            CarouselItem(1, R.drawable.ic_launcher_foreground, "donut"),
+            CarouselItem(2, R.drawable.ic_launcher_background, "eclair"),
+            CarouselItem(3, R.drawable.ic_launcher_foreground, "froyo"),
+            CarouselItem(4, R.drawable.ic_launcher_background, "gingerbread"),
+        )
+    }
+
+    HorizontalMultiBrowseCarousel(
+        state = rememberCarouselState { items.count() },
+        modifier = Modifier
+            .fillMaxWidth()
+            .wrapContentHeight()
+            .padding(top = 16.dp, bottom = 16.dp),
+        preferredItemWidth = 186.dp,
+        itemSpacing = 8.dp,
+        contentPadding = PaddingValues(horizontal = 16.dp)
+    ) { i ->
+        val item = items[i]
+        Image(
+            modifier = Modifier
+                .height(205.dp)
+                .maskClip(MaterialTheme.shapes.extraLarge),
+            painter = painterResource(id = item.imageResId),
+            contentDescription = item.contentDescription,
+            contentScale = ContentScale.Crop
+        )
+    }
+}
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    CURSOComposeTheme {
-        Greeting("Android")
-    }
+fun CarraPreview() {
+    CarouselExample_MultiBrowse(modifier = modifier)
 }
